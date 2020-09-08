6acbe854807b423d29253ec3e3edd81036cfbf7f
dougfraga <douglas.fraga@gmail.com>
2020-09-08 11:01:58 -0300

    Mock test implemented.

--- libpythonpro_doug/tests/test_spam/test_send_to_user_base.py
+++ libpythonpro_doug/tests/test_spam/test_send_to_user_base.py
@@ -1,20 +1,10 @@
+from unittest.mock import Mock
+
 import pytest
 
 from libpythonpro_doug.spam.email_sender import Sender
 from libpythonpro_doug.spam.main import SpamSender
 from libpythonpro_doug.spam.models import User
-
-
-class SenderMock(Sender):
-
-    def __init__(self):
-        super().__init__()
-        self.num_of_sent_emails = 0
-        self.sent_params = None
-
-    def send(self, shipper, recipient, title, body):
-        self.sent_params = (shipper, recipient, title, body)
-        self.num_of_sent_emails += 1
 
 
 @pytest.mark.parametrize(
@@ -32,28 +22,29 @@ class SenderMock(Sender):
 def test_number_of_spam(session, users):
     for user in users:
         session.save(user)
-    sender = SenderMock()
+    sender = Mock()
     spam_sender = SpamSender(session, sender)
     spam_sender.send_emails(
         'douglas.fraga@gmail.com',
         'Python Pro Training Course',
         'Email body'
     )
-    assert len(users) == sender.num_of_sent_emails
+    assert len(users) == sender.send.call_count
 
-    def test_spam_params(session):
-        user = User(name='Doug', email='douglas.fraga@gmail.com')
-        session.save(user)
-        sender = SenderMock()
-        spam_sender = SpamSender(session, sender)
-        spam_sender.send_emails(
-            'caroldanelli@gmail.com',
-            'Python Pro Training Course',
-            'Email body'
-        )
-        assert sender.sent_params == (
-            'caroldanelli@gmail.com',
-            'douglas.fraga@gmail.com'
-            'Python Pro Training Course',
-            'Email body'
-        )
+
+def test_spam_params(session):
+    user = User(name='Doug', email='douglas.fraga@gmail.com')
+    session.save(user)
+    sender = Mock()
+    spam_sender = SpamSender(session, sender)
+    spam_sender.send_emails(
+        'caroldanelli@gmail.com',
+        'Python Pro Training Course',
+        'Email body'
+    )
+    sender.send.assert_called_once_with(
+        'caroldanelli@gmail.com',
+        'douglas.fraga@gmail.com',
+        'Python Pro Training Course',
+        'Email body'
+    )
